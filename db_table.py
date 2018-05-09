567733bb0ce656e029b278bd4b3627a7a2f6c34c
xudi <xudi1989@ruc.edu.cn>
2018-05-09 09:15:27 +0800

    db_table regulize

--- db_table.py
+++ db_table.py
@@ -43,52 +43,38 @@ class CiticBank(DB_BASE):
     def get_col_sizes(self):
         return self.col_sizes
 
+def import_core(infile,table_name):
+    if infile.endswith('.csv'):
+        df = pd.read_csv(infile,encoding = 'utf8',index_col = None)
+    else:
+        df = pd.read_excel(infile,encoding = 'utf8',index_col = None)
+    df.columns = [ i.strip() for i in df.columns]
+    print df.columns
+    db_obj = CiticBank()
+    df.to_sql(table_name,db_obj.engine,chunksize = 10240,if_exists='append')
+    print 'finished'
+
 def import_reserve():
     infile = r'/media/xudi/coding/支付/深圳中信/5.4/上午/公司银行部/reserve.csv'
-    df = pd.read_csv(infile,encoding = 'utf8',index_col = 0)
-    print df.head()
-    db_obj = CiticBank()
-    df.to_sql('reserve',db_obj.engine,chunksize = 10240,if_exists='append')
-    print 'finished'
+    import_core(infile,'reserve')
     
 def import_online_account():
     infile = r'/media/xudi/coding/支付/深圳中信/5.7/2类户/线上开立2类户.csv'
-    df = pd.read_csv(infile,encoding = 'utf8',index_col = None)
-    print len(df)
-    db_obj = CiticBank()
-    df.to_sql('personal_account_second_online',db_obj.engine,chunksize = 10240,if_exists='append')
-    print 'finished'
+    import_core(infile,'personal_account_second_online')
     
 def import_offline_account():
     infile = r'/media/xudi/coding/支付/深圳中信/5.7/2类户/线下开立1,2类户_1.csv'
-    df = pd.read_csv(infile,encoding = 'utf8',index_col = None)
-    print len(df)
-    db_obj = CiticBank()
-    df.to_sql('personal_account_second_offline',db_obj.engine,chunksize = 10240,if_exists='append')
+    import_core(infile,'personal_account_second_offline')
     infile = r'/media/xudi/coding/支付/深圳中信/5.7/2类户/线下开立1,2类户_2.csv'
-    df = pd.read_csv(infile,encoding = 'utf8',index_col = None)
-    print len(df)
-    db_obj = CiticBank()
-    df.to_sql('personal_account_second_offline',db_obj.engine,chunksize = 10240,if_exists='append')
-    print 'finished'
+    import_core(infile,'personal_account_second_offline')
 
 def import_reserve_trade():
     infile = r'/media/xudi/coding/支付/深圳中信/5.7/附件6-备付金账户流水20180101-20180107.csv'
-    df = pd.read_csv(infile,encoding = 'utf8',index_col = None)
-    df.columns = [ i.strip() for i in df.columns]
-    print df.columns
-    db_obj = CiticBank()
-    df.to_sql('reserve_trade',db_obj.engine,chunksize = 10240,if_exists='append')
-    print 'finished'
+    import_core(infile,'reserve_trade')
 
 def import_citic_company_account():
     infile = r'/media/xudi/coding/支付/深圳中信/5.7/运营管理部/单位结算账户信息(账户管理系统）/开户_20180505_汇总.xls'
-    df = pd.read_excel(infile,encoding = 'utf8',index_col = None)
-    df.columns = [ i.strip() for i in df.columns]
-    print df.columns
-    db_obj = CiticBank()
-    df.to_sql('citic_company_account',db_obj.engine,chunksize = 10240,if_exists='append')
-    print 'finished'
+    import_core(infile,'citic_company_account')
     
 def import_pbc_company_account_log():
     infiles = [ r'/media/xudi/coding/支付/深圳中信/5.8/早上/20160503-20180503备案类数据记录（人行账户管理系统）/一般账户备案20160503-20170503.xls',
@@ -96,25 +82,35 @@ def import_pbc_company_account_log():
                ]
     for infile in infiles:
         print infile
-        df = pd.read_excel(infile,encoding = 'utf8',index_col = None)
-        df.columns = [ i.strip() for i in df.columns]
-        print df.columns
-        db_obj = CiticBank()
-        df.to_sql('pbc_company_account_log',db_obj.engine,chunksize = 10240,if_exists='append')
+        import_core(infile,'pbc_company_account_log')
     print 'finished'
-
 
 def import_pbc_account_system_status():
     infile = r'/media/xudi/coding/支付/深圳中信/5.7/运营管理部/单位结算账户信息(账户管理系统）/开户_20180505_汇总.xls'
-    df = pd.read_excel(infile,encoding = 'utf8',index_col = None)
-    df.columns = [ i.strip() for i in df.columns]
-    print df.columns
-    db_obj = CiticBank()
-    df.to_sql('pbc_account_system_status',db_obj.engine,chunksize = 10240,if_exists='append')
-    print 'finished'
+    import_core(infile,'pbc_account_system_status')
     
+def import_citic_bank_change_status():    
+    infile = r'/media/xudi/coding/支付/深圳中信/5.7/运营管理部/核心系统开户销户变更数据/变更.xls'
+    table_name = r'citic_account_change_status'
+    import_core(infile,table_name)
+    
+def import_pbc_banck_change_status():
+    infile = r'/media/xudi/coding/支付/深圳中信/5.8/早上/单位结算账户信息(账户管理系统）/变更_20180505_汇总.xls'
+    table_name = r'pbc_account_change_status'
+    import_core(infile,table_name)
+
+def import_citic_bank_delete_status():    
+    infile = r'/media/xudi/coding/支付/深圳中信/5.8/早上/核心系统开户销户变更数据/核心开销户.xlsx'
+    table_name = r'citic_account_delete_status'
+    import_core(infile,table_name)
+    
+def import_pbc_banck_delete_status():
+    infile = r'/media/xudi/coding/支付/深圳中信/5.8/早上/单位结算账户信息(账户管理系统）/销户20180505_汇总.xls'
+    table_name = r'pbc_account_delete_status'
+    import_core(infile,table_name)
     
 if __name__ == '__main__':
-    import_pbc_account_system_status()
+    import_pbc_banck_change_status()
+    import_pbc_banck_delete_status()
     
     
